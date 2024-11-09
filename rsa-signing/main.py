7c777387c6742ed0333476963630160f35f702d1
Igor <igor@sgf.lt>
2024-11-09 16:40:47 +0200

    Add verification

--- rsa-signing/main.py
+++ rsa-signing/main.py
@@ -2,7 +2,7 @@ import hashlib
 import time
 
 
-def read_private_key_from_file(filename: str, start_line: int) -> tuple[int, int]:
+def read_key_pair_from_file(filename: str, start_line: int) -> tuple[int, int]:
     with open(filename, "r") as f:
         lines = f.readlines()
         n = int(lines[start_line].split("=")[1].strip())
@@ -24,10 +24,16 @@ def create_signature(hash_value_hex: str, private_key: tuple[int, int]) -> int:
     signature = pow(message, d, n)
     return signature
 
+def verify_signature(hash_value_hex: str, signature: int, public_key: tuple[int, int]) -> bool:
+    n, e = public_key
+    decrypted_hash = pow(signature, e, n)
+    calculated_hash = int(hash_value_hex, 16)
+    return decrypted_hash == calculated_hash
 
 def main() -> None:
     filename = "certificate.txt"
-    private_key = read_private_key_from_file("rsa_keys.txt", 7)
+    private_key = read_key_pair_from_file("rsa_keys.txt", 7)
+    public_key = read_key_pair_from_file("rsa_keys.txt", 3)
 
     hash = hash_file(filename)
     signature = create_signature(hash, private_key)
@@ -47,6 +53,11 @@ def main() -> None:
 
     with open("output.txt", "r") as f:
         print(f.read())
+
+    is_valid = verify_signature(hash, signature, public_key)
+    print("Parašo patvirtinimo rezultatas:", "Patvirtintas" if is_valid else "Nepatvirtintas")
+    is_valid = verify_signature(combined_hash, combined_signature, public_key)
+    print("Parašo su laiko žyma patvirtinimo rezultatas:", "Patvirtintas" if is_valid else "Nepatvirtintas")
 
 
 if __name__ == "__main__":
